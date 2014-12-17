d322c3e7a8191cc71862a0031ae40c47dbcb782a
Arsalan Shah <manactric@gmail.com>
2014-12-17 13:01:33 +0500

    Profile Access Intital Release

---
+++ locale/ossn.en.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network
+ * @author    Open Social Website Core Team <info@informatikon.com>
+ * @copyright 2014 iNFORMATIKON TECHNOLOGIES
+ * @license   General Public Licence http://www.opensource-socialnetwork.org/licence
+ * @link      http://www.opensource-socialnetwork.org/licence
+ */
+
+$en = array(
+    'profile:access:error' => 'You need to login before view that page',
+);
+ossn_register_languages('en', $en); 
